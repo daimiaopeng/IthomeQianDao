1e489e0369271381d6da0a100f38fa8455643d7f
daimiaopeng <32475719+daimiaopeng@users.noreply.github.com>
2020-06-30 15:02:04 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,9 +2,27 @@ import requests
 import json
 import re
 import datetime
-import hashlib
-#使用说明：安装python3，再在cmd里输入pip install requests 然后改动下面数据就可以了日志文件保存
-#在同目录下的log.txt
+import Crypto.Cipher.DES
+
+# 更新时间 2020年6月30日14:48:58
+# 使用说明：安装python3，再在cmd里输入pip install requests 然后改动下面数据就可以了日志文件保存
+# 在同目录下的log.txt
+def auto_fill(x):
+    if len(x) > 24:
+        raise "密钥长度不能大于等于24位！"
+    else:
+        while len(x) < 32:
+            x += "\0"
+        return x.encode()
+
+
+def getHash(text):
+    key = "(#i@x*l%"
+    x = Crypto.Cipher.DES.new(key.encode(), Crypto.Cipher.DES.MODE_ECB)
+    a = x.encrypt(auto_fill(str(text))).hex()
+    return str(a)
+
+
 def run(username, password):
     session = requests.session()
     url_login = 'http://my.ruanmei.com/Default.aspx/LoginUser'
@@ -27,28 +45,21 @@ def run(username, password):
     try:
         response = session.post(url=url_login, data=json.dumps(data), headers=header).headers['Set-Cookie']
         user_hash = re.search(r'user=hash=[a-zA-Z0-9]{160,160}', response).group()[10:]
-        md5 = hashlib.md5(str(datetime.datetime.now()).encode('utf-8')).hexdigest()
-        url_qiandao = 'http://my.ruanmei.com/api/UserSign/Sign?userHash='
-        url_pingbi = 'http://dyn.ithome.com/api/user/getblockuser?userhash=%s' % user_hash
-        url_qiandao = 'http://my.ruanmei.com/api/UserSign/Sign?userHash=%s&type=0&endt=%s' % (user_hash,md5)
-        # pingbi = session.get(url=url_pingbi).json()
+        md5 = getHash(str(datetime.date.today()))
+        url_qiandao = 'http://my.ruanmei.com/api/UserSign/Sign?userHash=%s&type=0&endt=%s' % (user_hash, md5)
         qiandao = session.get(url=url_qiandao).json()
         with open('log.txt', 'a', encoding='utf-8') as f:
             f.write(str(datetime.datetime.now()) + ' ' + username + ' ' + str(qiandao['msg']) + '\n')
         print(qiandao)
     except:
         print("用户名或密码错误")
-        return 0
 
 
 my_list = [
     {
-        'username': '账号1',
+        'username': '用户名',
         'password': '密码',
-    }, {
-        'username': '账号2',
-        'password': '密码',
-    }, 
+    }
 ]
 for i in my_list:
     code = run(i['username'], i['password'])
