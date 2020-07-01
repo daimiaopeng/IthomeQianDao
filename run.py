1248501498df162e4cb6d6e16a047de4735ccb40
daimiaopeng <32475719+daimiaopeng@users.noreply.github.com>
2020-07-01 16:49:12 +0800

    添加其它地方签到

--- run.py
+++ run.py
@@ -3,10 +3,12 @@ import json
 import re
 import datetime
 import Crypto.Cipher.DES
+requests.packages.urllib3.disable_warnings()
 
-# 更新时间 2020年6月30日14:48:58
+# 更新时间 2020年7月1日16:48:46
 # 使用说明：安装python3，再在cmd里输入pip install requests 然后改动下面数据就可以了日志文件保存
 # 在同目录下的log.txt
+
 def auto_fill(x):
     if len(x) > 24:
         raise "密钥长度不能大于等于24位！"
@@ -24,35 +26,35 @@ def getHash(text):
 
 
 def run(username, password):
-    session = requests.session()
     url_login = 'https://my.ruanmei.com/Default.aspx/LoginUser'
     data = {'mail': username, 'psw': password, 'rememberme': 'true'}
     header = {
         'Accept': 'application/json, text/javascript, */*; q=0.01',
         'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive',
-        'Content-Length': '60',
         'Content-Type': 'application/json; charset=UTF-8',
         'Host': 'my.ruanmei.com',
         'Origin': 'http://my.ruanmei.com',
-        'Pragma': 'no-cache',
         'Referer': 'http://my.ruanmei.com/',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
         'X-Requested-With': 'XMLHttpRequest',
     }
     try:
-        response = session.post(url=url_login, data=json.dumps(data), headers=header).headers['Set-Cookie']
+        response = requests.post(url=url_login, data=json.dumps(data), headers=header).headers['Set-Cookie']
         user_hash = re.search(r'user=hash=[a-zA-Z0-9]{160,160}', response).group()[10:]
-        md5 = getHash(str(datetime.date.today()))
-        url_qiandao = 'https://my.ruanmei.com/api/UserSign/Sign?userHash=%s&type=0&endt=%s' % (user_hash, md5)
-        qiandao = session.get(url=url_qiandao).json()
-        with open('log.txt', 'a', encoding='utf-8') as f:
-            f.write(str(datetime.datetime.now()) + ' ' + username + ' ' + str(qiandao['msg']) + '\n')
-        print(qiandao)
-    except:
-        print("用户名或密码错误")
+        endt = getHash(str(datetime.date.today()))
+        session = requests.session()
+        session.verify=False
+        for fuck in range(0, 400):
+            url_qiandao = 'https://my.ruanmei.com/api/usersign/yunrilisign?userHash=%s&coinHistoryType=%s&endt=%s' % (user_hash, fuck, endt)
+            try:
+                qiandao = session.get(url=url_qiandao).json()
+                print(qiandao)
+            except Exception as e:
+                print(e)
+                continue
+    except Exception as e:
+        print(e)
+        print("可能密码错误")
 
 
 my_list = [
