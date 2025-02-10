980c1ac56fce30bae77eaf616b0a33fe1a08e2a6
wherewhere <qq2518766683@outlook.com>
2025-02-10 14:56:14 +0800

    修复 UserHash 获取错误
    添加直接使用 UserHash 签到

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ requests.packages.urllib3.disable_warnings()
 # 在同目录下的log.txt
 def make_up_data(data: str, mode="PAD_ZERO"):
     pad = 8 - len(data) % 8
-    pad_str = ''
+    pad_str = ""
     if mode == "PAD_PKCS5":
         for _ in range(pad):
             pad_str += chr(pad)
@@ -23,20 +23,6 @@ def make_up_data(data: str, mode="PAD_ZERO"):
         for _ in range(pad):
             pad_str += chr(0)
     return data + pad_str
-
-
-def des_encrypt(s: str):
-    """
-    DES 加密
-    :param s: 原始字符串
-    :return: 加密后字符串，16进制
-    """
-    s = make_up_data(s)
-    secret_key = KEY
-    iv = secret_key
-    k = des(secret_key, ECB, iv, pad=None)
-    en = k.encrypt(s)
-    return binascii.b2a_hex(en)
 
 
 def des_encrypt2(s: str, key: str):
@@ -51,22 +37,6 @@ def des_encrypt2(s: str, key: str):
     k = des(secret_key, ECB, iv, pad=None)
     en = k.encrypt(s)
     return binascii.b2a_hex(en).decode()
-
-
-def des_descrypt(s: str) -> str:
-    """
-    DES 解密
-    :param s: 加密后的字符串，16进制
-    :return:  解密后的字符串
-    """
-    secret_key = KEY
-    iv = secret_key
-    k = des(secret_key, ECB, iv, pad=None)
-    de = k.decrypt(binascii.a2b_hex(s))
-    return de
-
-
-# print(des_descrypt("b1d5d07310f93d040d19ee09ce9edfc106da4ace88148d05"))
 
 
 def cn(x: bytes):
@@ -152,7 +122,7 @@ def geKK2(_0x249526: int, _0x2fce26: int):
 
 
 def gneKK(_0x4060f6: datetime):
-    return 'k' + des_encrypt2(geKK(_0x4060f6, 3), geKK(_0x4060f6, 8))
+    return "k" + des_encrypt2(geKK(_0x4060f6, 3), geKK(_0x4060f6, 8))
 
 
 def geKsK(_0x101b3e: datetime):
@@ -160,7 +130,7 @@ def geKsK(_0x101b3e: datetime):
 
 
 def gneKK2(_0x4060f6: int):
-    return 'k' + des_encrypt2(geKK2(_0x4060f6, 3), geKK2(_0x4060f6, 8))
+    return "k" + des_encrypt2(geKK2(_0x4060f6, 3), geKK2(_0x4060f6, 8))
 
 
 def geKsK2(_0x101b3e: float):
@@ -172,10 +142,32 @@ def geKsK2(_0x101b3e: float):
 
 def getSign():
     now = int(time.time() * 1000)
-    return "&timestamp=" + str(now) + '&' + gneKK2(now) + '=' + geKsK2(now)
+    return "&timestamp=" + str(now) + "&" + gneKK2(now) + "=" + geKsK2(now)
 
 
-def run(username: str, password: str):
+def getUserHash(username: str, password: str):
+    try:
+        url_login = "https://my.ruanmei.com/Default.aspx/LoginUser"
+        data = {"mail": username, "psw": password, "rememberme": "true"}
+        header = {
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "Accept-Encoding": "gzip, deflate",
+            "Content-Type": "application/json; charset=UTF-8",
+            "Host": "my.ruanmei.com",
+            "Origin": "http://my.ruanmei.com",
+            "Referer": "http://my.ruanmei.com/",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+            "X-Requested-With": "XMLHttpRequest",
+        }
+        response = requests.post(
+            url=url_login, data=json.dumps(data), headers=header
+        ).headers["Set-Cookie"]
+        return re.search(r"user=hash=[a-zA-Z0-9]+", response).group()[10:]
+    except Exception as e:
+        raise ExceptionGroup("可能密码错误", [e])
+
+
+def signWithHash(user_hash: str):
     qiandaocode = [
         0,
         # 1,
@@ -194,67 +186,52 @@ def run(username: str, password: str):
         # 770,
         # 771
     ]
-    url_login = "https://my.ruanmei.com/Default.aspx/LoginUser"
-    data = {"mail": username, "psw": password, "rememberme": "true"}
-    header = {
-        "Accept": "application/json, text/javascript, */*; q=0.01",
-        "Accept-Encoding": "gzip, deflate",
-        "Content-Type": "application/json; charset=UTF-8",
-        "Host": "my.ruanmei.com",
-        "Origin": "http://my.ruanmei.com",
-        "Referer": "http://my.ruanmei.com/",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
-        "X-Requested-With": "XMLHttpRequest",
+    endt = getSign()
+    session = requests.session()
+    session.verify = False
+    session.headers = {
+        "user-agent": "Mozilla/5.0 (Linux; Android 9; MI 6 Build/PKQ1.190118.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.138 Mobile Safari/537.36 ithome/rmsdklevel2/night/7.26",
+        "content-type": "application/x-www-form-urlencoded",
+        "accept": "*/*",
+        "x-requested-with": "com.ruanmei.ithome",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-dest": "empty",
+        "referer": "https://my.ruanmei.com/app/user/signin.html?hidemenu=1&appver=2",
     }
-    try:
-        response = requests.post(
-            url=url_login, data=json.dumps(data), headers=header
-        ).headers["Set-Cookie"]
-        user_hash = re.search(r"user=hash=[a-zA-Z0-9]{160,160}", response).group()[10:]
-        endt = getSign()
-        session = requests.session()
-        session.verify = False
-        session.headers = {
-            "user-agent": "Mozilla/5.0 (Linux; Android 9; MI 6 Build/PKQ1.190118.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.138 Mobile Safari/537.36 ithome/rmsdklevel2/night/7.26",
-            "content-type": "application/x-www-form-urlencoded",
-            "accept": "*/*",
-            "x-requested-with": "com.ruanmei.ithome",
-            "sec-fetch-site": "same-origin",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-dest": "empty",
-            "referer": "https://my.ruanmei.com/app/user/signin.html?hidemenu=1&appver=2",
-        }
-        error: list[Exception] = []
-        for fuck in qiandaocode:
+    error: list[Exception] = []
+    for fuck in qiandaocode:
+        try:
             url_qiandao = f"https://my.ruanmei.com/api/usersign/sign?userHash={user_hash}&type={fuck}&endt={endt}"
-            try:
-                qiandao = session.get(url=url_qiandao).json()
-                print(qiandao)
-                if "msg" in qiandao:
-                    msg: str = qiandao["msg"]
-                    if msg.find("失败"):
-                        error.append(Exception(qiandao))
-            except Exception as e:
-                error.append(e)
-        return error
-    except Exception as e:
-        raise ExceptionGroup("可能密码错误", [e])
+            qiandao = session.get(url=url_qiandao).json()
+            print(f"签到 (type {fuck})：{qiandao}")
+            if "msg" in qiandao:
+                msg: str = qiandao["msg"]
+                if msg.find("失败") != -1:
+                    error.append(Exception(qiandao))
+            else:
+                error.append(Exception("签到失败，可能是 UserHash 错误"))
+        except Exception as e:
+            error.append(e)
+    return error
 
 
-my_list = [
-    {
-        "username": "用户名",
-        "password": "密码",
-    }
-]
-for i in my_list:
-    if "USERNAME" not in ENV or "PASSWORD" not in ENV:
-        raise Exception("未配置环境变量 USERNAME 或 PASSWORD")
-    else:
-        i["username"] = ENV["USERNAME"]
-        i["password"] = ENV["PASSWORD"]
-        error = run(i["username"], i["password"])
+if __name__ == "__main__":
+    if "USERHASH" in ENV:
+        print("通过 UserHash 签到")
+        error = signWithHash(ENV["USERHASH"])
         if len(error):
             raise ExceptionGroup("签到失败", error)
         else:
             print("签到成功")
+    elif "USERNAME" in ENV and "PASSWORD" in ENV:
+        print(f"登录 {ENV["USERNAME"]}")
+        user_hash = getUserHash(ENV["USERNAME"], ENV["PASSWORD"])
+        print("登录成功，开始签到")
+        error = signWithHash(user_hash)
+        if len(error):
+            raise ExceptionGroup("签到失败", error)
+        else:
+            print("签到成功")
+    else:
+        raise Exception("请配置环境变量 USERNAME 与 PASSWORD，或 USERHASH")
